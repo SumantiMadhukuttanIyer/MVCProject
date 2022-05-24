fb525ebcaf88f9260cad82023a1a33a27101303f
SHRADDHA <SHRADDHA@LAPTOP-8OGRSAUK>
2022-05-24 14:52:05 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using MVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            var productModel = new ProductModel();
+            ViewBag.products = productModel.findAll();
+            return View();
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MVCProject.Models
+{
+    public class ProductModel
+    {
+        internal Product find(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal dynamic findAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
